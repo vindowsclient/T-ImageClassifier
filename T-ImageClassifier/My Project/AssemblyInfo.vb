5400f1ba4908e9f560419b78797e7b11efcd7eb0
Vinod Nair <nairv@Riversand.com>
2019-08-30 00:36:13 -0500

    Add project files.

---
+++ T-ImageClassifier/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("T_ImageClassifier")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_ImageClassifier")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("420b22a5-0d9a-4431-b274-420e70e46642")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ T-ImageClassifier/index.js
@@ -0,0 +1,126 @@
+﻿let net;
+const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
+
+async function setupWebcam() {
+    return new Promise((resolve, reject) => {
+        const navigatorAny = navigator;
+        navigatorAny.getUserMedia = navigatorAny.getUserMedia ||
+            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+            navigatorAny.msGetUserMedia;
+        if (navigatorAny.getUserMedia) {
+            navigatorAny.getUserMedia({ video: true },
+                stream => {
+                    webcamElement.srcObject = stream;
+                    webcamElement.addEventListener('loadeddata', () => resolve(), false);
+                },
+                error => reject());
+        } else {
+            reject();
+        }
+    });
+}
+
+
+function addTable() {
+    var myTableDiv = document.getElementById("myDynamicTable");
+   // myTableDiv.removeChild();
+
+    var table = document.createElement('TABLE');
+    table.border = '1';
+    //table.children().remove();
+    
+    var th1 = document.createElement('TH');
+    th1.appendChild(document.createTextNode("Prediction"));
+    table.appendChild(th1);
+
+    var th2 = document.createElement('TH');
+    th2.appendChild(document.createTextNode("Probability"));
+    table.appendChild(th2);
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    //for (var i = 0; i < 5; i++) {
+    //    var tr = document.createElement('TR');
+    //    tableBody.appendChild(tr);
+
+    //    var td1 = document.createElement('TD');
+    //    td1.width = '75';
+    //    var val1 = '${ result[i].className }';
+    //    td1.appendChild(document.createTextNode(val1));
+    //    tr.appendChild(td1);
+
+    //    var td2 = document.createElement('TD');
+    //    td2.width = '75';
+    //    var val2 ='${ result[i].probability }'
+    //    td2.appendChild(document.createTextNode(val2));
+    //    tr.appendChild(td2);
+       
+
+    //    //for (var j = 0; j < 2; j++) {
+    //    //    var td = document.createElement('TD');
+    //    //    td.width = '75';
+    //    //    td.appendChild(document.createTextNode("Cell " + i + "," + j));
+    //    //    tr.appendChild(td);
+    //    //}
+    //}
+    myTableDiv.appendChild(table);
+    return tableBody;
+}
+//addTable();
+
+
+async function app() {
+    console.log('Loading mobilenet..');
+
+    // Load the model.
+    net = await mobilenet.load();
+    console.log('Sucessfully loaded model');
+
+    await setupWebcam();
+    var tableBody = addTable();
+    
+    while (true) {
+        const result = await net.classify(webcamElement);
+        
+
+        for (var i = 0; i < result.length; i++) {
+            tableBody.children[i];
+            
+            var tr = document.createElement('TR');
+            tableBody.appendChild(tr);
+
+            var td1 = document.createElement('TD');
+            td1.width = '175';
+            var val1 = result[i].className; 
+            td1.appendChild(document.createTextNode(val1));
+            tr.appendChild(td1);
+
+            var td2 = document.createElement('TD');
+            td2.width = '75';
+            var val2 = result[i].probability;
+            td2.appendChild(document.createTextNode(val2));
+            tr.appendChild(td2);
+
+
+            //for (var j = 0; j < 2; j++) {
+            //    var td = document.createElement('TD');
+            //    td.width = '75';
+            //    td.appendChild(document.createTextNode("Cell " + i + "," + j));
+            //    tr.appendChild(td);
+            //}
+        }
+      //  document.getElementById('console').innerText = `
+      //prediction: ${result[0].className}\n
+      //probability: ${result[0].probability}
+        //`;
+       // addTable(result);
+        // Give some breathing room by waiting for the next animation frame to
+        // fire.
+        
+        await tf.nextFrame();
+    }
+}
+
+app();
